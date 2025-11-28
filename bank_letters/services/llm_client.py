b4ff7517ee8bf32e6d6c8f4ccef7eb7491dded6f
YKudrin <xbhgbz@gmail.com>
2025-11-28 16:45:52 +0300

    add_llm_script

--- bank_letters/services/llm_client.py
+++ bank_letters/services/llm_client.py
@@ -1,21 +1,51 @@
 import requests
+import os
 from django.conf import settings
+from dotenv import load_dotenv
+from openai import OpenAI
+from models import RequestAnalysis
+from prompts import EMAIL_ANALYSIS_PROMPT
+from openai.types.chat import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
 
+BASE_LLM_URL = 'https://rest-assistant.api.cloud.yandex.net/v1'
+QWEN3_235B_MODEL_NAME = 'qwen3-235b-a22b-fp8/latest'
+YAGPT_MODEL_NAME = 'yandexgpt/rc'
 
 class LLMClient:
     def __init__(self):
+        load_dotenv()
+        self.folder_id = os.environ['folder_id']
+        self.api_key = os.environ['api_key']
         self.api_url = settings.LLM_API_URL
-        self.api_key = settings.LLM_API_KEY
 
-    def analyze_letter(self, text):
-        # Запрос к API нейросети для анализа письма
-        response = requests.post(
-            self.api_url,
-            headers={'Authorization': f'Bearer {self.api_key}'},
-            json={'prompt': f'Проанализируй текст: {text}'}
-        )
-        return response.json()
+    def make_model(self, model_name):
+        return f"gpt://{self.folder_id}/{model_name}"
+
+    # Запрос к API нейросети для анализа письма
+    def analyze_email(self, text):
+        model = self.make_model(model_name=YAGPT_MODEL_NAME)
+        client = OpenAI(
+            base_url = "https://rest-assistant.api.cloud.yandex.net/v1",
+            api_key = self.api_key, project = self.folder_id)
+
+        try:
+            response = client.beta.chat.completions.parse(
+                model=model,
+                messages=[
+                    ChatCompletionSystemMessageParam(role="system", content=EMAIL_ANALYSIS_PROMPT),
+                    ChatCompletionUserMessageParam(role="user", content=text)
+                ],
+                response_format=RequestAnalysis,
+                temperature=0.1
+            )
+
+            return response.choices[0].message.parsed
+        except Exception as e:
+            print(f"Ошибка при анализе запроса: {e}")
 
     def generate_responses(self, letter_analysis):
         # Генерация вариантов ответов
         pass
+
+
+
---
+++ bank_letters/services/models.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+from enum import Enum
+
+class TopicCategory(str, Enum):
+    INFORMATION_REQUEST = 'Запрос информации/документов'
+    COMPLAINT = 'Официальная жалоба или претензия'
+    GOVERNMENT_REQUEST = 'Регуляторный запрос'
+    PARTNER_DEAL =  'Партнёрское предложение'
+    APPROVAL_REQUEST =  'Запрос на согласование'
+    NOTICE =  'Уведомление или информирование'
+    OTHER = "Другое"
+
+# Определение стилей ответа
+class ResponseStyle(str, Enum):
+    OFFICIAL = 'Строгий официальный стиль'
+    CORPORATE = 'Деловой корпоративный стиль'
+    CLIENT = 'Клиентоориентированный вариант'
+    INFORMATION =  'Краткий информационный ответ'
+
+# Определение уровней критичности
+class CriticalityLevel(str, Enum):
+    LOW = "Низкий"
+    MEDIUM = "Средний"
+    HIGH = "Высокий"
+    CRITICAL = "Критичный"
+
+class RequestAnalysis(BaseModel):
+    topic_category: TopicCategory = Field(
+        description="Классификация темы по категориям"
+    )
+    response_style: ResponseStyle = Field(
+        description="Предпочтительный стиль ответа"
+    )
+    processing_time_hours: float = Field(
+        ge=0.1,
+        le=1720,
+        description="Время на обработку в часах согласно регламенту"
+    )
+    criticality_level: CriticalityLevel = Field(
+        description="Уровень критичности запроса"
+    )
+    summary: str = Field(
+        max_length=500,
+        description="Краткое содержание запроса"
+    )
